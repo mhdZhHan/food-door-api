75a36811b56e9c9fb150da06e8877463414bdf8c
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-05-24 10:27:55 +0530

    Update static path

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('static'));
 
 
 // coustom middlewares
